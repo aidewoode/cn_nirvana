10b15cfd20485d6a5e7341ab65e895f8dec3542b
aidewoode <aidewoode@gmail.com>
2014-08-26 23:57:39 +0800

    修复问题

--- environments.rb
+++ environments.rb
@@ -4,7 +4,7 @@ end
 
 configure :production do
   db = URI.parse("postgres://ntowdmawnahglf:kP6ymEOZUBRce5Anbuzp5o5ELC@ec2-54-204-24-154.compute-1.amazonaws.com:5432/ddbjfgc23a5kqi")
-
+# 需要改进，不要把数据库链接暴露出来。
   ActiveRecord::Base.establish_connection(
     :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
     :host => db.host,
