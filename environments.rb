89be5c96e6fe520f2f1da95e02463637a2d2ca8b
aidewoode <aidewoode@gmail.com>
2014-08-27 21:42:21 +0800

    修复数据库访问的一个安全问题

--- environments.rb
+++ environments.rb
@@ -3,7 +3,7 @@ configure :development do
 end
 
 configure :production do
-  db = URI.parse("postgres://ntowdmawnahglf:kP6ymEOZUBRce5Anbuzp5o5ELC@ec2-54-204-24-154.compute-1.amazonaws.com:5432/ddbjfgc23a5kqi")
+  db = URI.parse(ENV['DATABASE_URL'])
 # 需要改进，不要把数据库链接暴露出来。
   ActiveRecord::Base.establish_connection(
     :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
