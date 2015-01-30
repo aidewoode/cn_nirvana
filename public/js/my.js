97f78dd4a9bd9c2aebad0df008cacb65e4efbc7a
aidewoode <aidewoode@gmail.com>
2015-01-30 15:11:55 +0800

    fixed some issue in my.js file

--- public/js/my.js
+++ public/js/my.js
@@ -1,26 +1,28 @@
 (function dropList() {
   var click = document.querySelector(".drop_down");
   var list = document.querySelector(".drop_menu");
-  click.onclick = function(event) {
-    list.classList.add("drop_active");
-    event.stopPropagation();
-  };
+  if (click) {
+    click.onclick = function(event) {
+      list.classList.add("drop_active");
+      event.stopPropagation();
+    };
 
-  document.body.onclick = function(event) {
-    list.classList.remove("drop_active");
-  };
+    document.body.onclick = function(event) {
+      list.classList.remove("drop_active");
+    };
   
-})();
+}})();
 
 (function changeCommentRoute() {
   var post = document.querySelector(".comment_submit");
-  post.onclick = function() {
-    var atwho = document.querySelector("a.simditor-mention");
-    if(atwho.dataset.mention) {
-      var attr = atwho.getAttribute("href");
-      var postForm = document.querySelector("form.comment");
-      postForm.setAttribute("action",postForm.getAttribute("action") + attr.slice(8));
-    } 
-    
-  };
-})();
+  if (post) {
+    post.onclick = function() {
+      var atwho = document.querySelector("a.simditor-mention");
+      if(atwho.dataset.mention) {
+        var attr = atwho.getAttribute("href");
+        var postForm = document.querySelector("form.comment");
+        postForm.setAttribute("action",postForm.getAttribute("action") + attr.slice(8));
+      } 
+
+    };
+}})();
