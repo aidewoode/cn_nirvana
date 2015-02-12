fa384267172242f72b2166526afc71dbb5e8dcfb
aidewoode <aidewoode@gmail.com>
2015-02-12 12:53:14 +0800

    add a checkout function in User sign up page before the submit

--- app.rb
+++ app.rb
@@ -120,7 +120,8 @@ class User < ActiveRecord::Base
 
   before_save { |user| user.email = email.downcase ; user.name = name.downcase }
 
-  validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+  VALID_NAME = /\A\w+\z/
+  validates :name, presence: true, format: { with: VALID_NAME }, length: { maximum: 25 }, uniqueness: { case_sensitive: false }
   VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL },uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }, on: :create
--- public/js/my.js
+++ public/js/my.js
@@ -27,16 +27,129 @@
     };
 }})();
 
+
+function mycheckInput(form) {
+  var reallyTrue = [];
+  for (i = 0; i < form.elements.length - 1; i++) {
+    switch (i) {
+      case 0:
+        var element = form.elements[0];
+        var input = element.value;
+        if (input === "") {
+          element.classList.add("error");
+          element.setAttribute("placeholder", "ID 不能为空");
+          reallyTrue.push(false);
+        }else {
+          var legalId = /^\w+$/;
+          if (legalId.test(input)) {
+            reallyTrue.push(true);
+            element.classList.remove("error");
+            element.setAttribute("placeholder", "只能为数字和字母的组合");
+          } else {
+            element.classList.add("error");
+            element.setAttribute("placeholder", "只能为数字和字母的组合");
+            reallyTrue.push(false);
+          }
+        }
+        break;
+      case 1:
+        var element = form.elements[1];
+        var input = element.value;
+        if (input === "") {
+          element.classList.add("error");
+          element.setAttribute("placeholder", "Email 不能为空");
+          reallyTrue.push(false);
+        }else {
+          var legalEmail = /^[a-z0-9](\w|\.|-)*@([a-z0-9]+-?[a-z0-9]+\.){1,3}[a-z]{2,4}$/i;
+          if (legalEmail.test(input)) {
+            reallyTrue.push(true);
+            element.classList.remove("error");
+            element.setAttribute("placeholder", "注册后不能更改");
+          } else {
+            element.classList.add("error");
+            element.setAttribute("placeholder", "Email 格式不正确");
+            reallyTrue.push(false);
+          }
+        }
+
+        break;
+      case 2:
+        var element = form.elements[2];
+        var input = element.value;
+        if (input === "") {
+          element.classList.add("error");
+          element.setAttribute("placeholder", "密码不能为空");
+          reallyTrue.push(false);
+        }else {
+          if (input.length >= 6) {
+            reallyTrue.push(true);
+            element.classList.remove("error");
+            element.removeAttribute("placeholder");
+          } else {
+            element.classList.add("error");
+            element.setAttribute("placeholder", "密码最少6 位");
+            reallyTrue.push(false);
+          }
+        }
+        break;
+      case 3:
+        var element = form.elements[3];
+        var input = element.value;
+        if (input === "") {
+          element.classList.add("error");
+          element.setAttribute("placeholder", "密码不能为空");
+          reallyTrue.push(false);
+        }else {
+          if (input === form.elements[2].value) {
+            reallyTrue.push(true);
+            element.classList.remove("error");
+            element.removeAttribute("placeholder");
+          } else {
+            element.classList.add("error");
+            element.setAttribute("placeholder", "密码不相同");
+            reallyTrue.push(false);
+          }
+        }
+
+        break;
+    }
+
+  }
+    if (reallyTrue.every(function(item, index, array) {
+      return item;
+    })) {
+      return true;
+    } else {
+      form.reset();
+      return false;
+    }
+}
+
 (function formSubmit() {
   var form = document.querySelector("form.submit_form");
   var input = document.querySelector("input.submit_form");
   if (form) {
-    form.onsubmit = function() {
-      input.setAttribute("disabled", "disabled");
-      input.classList.add("disable");
-    };
-  } 
-})();
+    if (form.classList[0] === "signup_form") {
+      var changeSubmitButton = function(event) {
+        input.setAttribute("disabled", "disabled");
+        input.classList.add("disable");
+        if (!mycheckInput(form)) {
+          input.removeAttribute("disabled");
+          input.classList.remove("disable");
+          event.preventDefault();
+          form.removeEventListener("submit", changeSubmitButton,false);
+          form.addEventListener("submit", changeSubmitButton, false);
+        }
+      };
+      form.addEventListener("submit", changeSubmitButton, false);
+
+    } else {
+      form.onsubmit = function() {
+        input.setAttribute("disabled", "disabled");
+        input.classList.add("disable");
+      };
+    }
+  }})();
 
 // moment.js function
 //
@@ -65,3 +178,4 @@
     }
   }
 })();
+
